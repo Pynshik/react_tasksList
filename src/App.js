f6e2206b046793ef3a90ecda11c03b4a548afda9
Pynshik <60570875+Pynshik@users.noreply.github.com>
2020-12-28 14:16:01 +0300

    React with functional components

--- src/App.js
+++ src/App.js
@@ -1,65 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import AddTask from "./AddTask";
 import TasksList from "./Tasks/TasksList";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      taskList: [],
-      edit: false,
-      changeableTask: {},
-    };
-  }
+function App() {
+  const [TaskList, SetTaskList] = useState([]);
+  const [IsEdit, SetIsEdit] = useState(false);
+  const [ChangeableTask, SetChangeableTask] = useState({});
 
-  addNewTask = (newTask) => {
-    this.setState((state) => {
-      state.taskList.push(newTask);
-      return state;
-    });
+  function addTaskOrEdit(newTask) {
+    let tasks = Object.assign([], TaskList);
+    let existTask = tasks.find(task => task.id === newTask.id);
+
+    if (existTask) {
+      let existIndex = tasks.indexOf(existTask);
+      tasks[existIndex] = newTask;
+      SetTaskList(tasks);
+      SetIsEdit(false)
+    }
+    else {
+      tasks.push(newTask);
+      SetTaskList(tasks);
+    }
   };
 
-  deleteTask = (id) => {
-    const taskIndex = this.state.taskList.map((task) => task.id).indexOf(id);
+  function deleteTask(id) {
+    const taskIndex = TaskList.map((task) => task.id).indexOf(id);
     const assurance = window.confirm('Do yoy want to delete this task?');
-    if(!assurance) return;
-    this.setState((state) => {
-      delete state.taskList[taskIndex];
-      return state;
-    });
+    if (!assurance) return;
+    let tasks = Object.assign([], TaskList);
+    delete tasks[taskIndex];
+    SetTaskList(tasks);
   };
 
-  editTask = (id) => {
-    console.log(id);
-    const taskIndex = this.state.taskList.map((task) => task.id).indexOf(id);
-    console.log(taskIndex)
-    this.setState(state => {
-      state.edit = true;
-      state.changeableTask = state.taskList[taskIndex];
-      return state;
-    });
-    console.log(this.state);
+  const handleEditBtnClick = (id) => {
+    const taskIndex = TaskList.map((task) => task.id).indexOf(id);
+    let tasks = Object.assign([], TaskList);
+    SetChangeableTask(tasks[taskIndex]);
+    SetIsEdit(true);
   };
 
-  render() {
-    console.log(this.state.edit)
-    console.log(this.state.changeableTask)
-    return (
-      <div className="wrapper">
-        <h1>Tasks list</h1>
-        <AddTask
-          edit={this.state.edit}
-          changeableTask={this.state.changeableTask}
-          addNewTask={this.addNewTask}
-        />
-        <TasksList
-          edit={this.editTask}
-          delete={this.deleteTask}
-          tasks={this.state.taskList}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="wrapper">
+      <h1>Tasks list</h1>
+      <AddTask
+        edit={IsEdit}
+        changeableTask={ChangeableTask}
+        addTaskOrEdit={addTaskOrEdit}
+      />
+      <TasksList
+        edit={handleEditBtnClick}
+        delete={deleteTask}
+        tasks={TaskList}
+      />
+    </div>
+  );
 }
 
 export default App;
