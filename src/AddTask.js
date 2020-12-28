43b8f2ccf14c0179531d9bc9c6a8f1756f96d9e5
Pynshik <60570875+Pynshik@users.noreply.github.com>
2020-12-28 14:14:42 +0300

    React with functional components

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,9 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const styles = {
-  form: {
-    display: "none",
-  },
   input: {
     width: "93%",
     margin: "8px 0",
@@ -25,223 +22,228 @@ const styles = {
   }
 };
 
+function AddTask(props) {
+  const [Task, SetTask] = useState({});
+  const [FormHidden, SetFormHidden] = useState(true);
+  const [OtherFieldsVisible, SetOtherFieldsVisible] = useState(true);
+  const [IsEdit, SetIsEdit] = useState(false);
 
-function valideteEmail(regex, el){
-    return regex.test(el);
-}
+  const [firstNameErr, setFirstNameErr] = useState({});
+  const [lastNameErr, setLastNameErr] = useState({});
+  const [emailErr, setEmailErr] = useState({});
 
-class AddTask extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      task: {
-        id: Math.random(7),
-        firstName: "",
-        lastName: "",
-        email: "",
-        fromDate: new Date(),
-        toDate: new Date(),
-        type: "",
-        report: false,
-        comment: "",
-      },
-      formVisible: true,
-      otherFields: true,
-    };
+  useEffect(
+    () => {
+      if (props.edit) {
+        SetFormHidden(false)
+        SetTask(props.changeableTask)
+        SetIsEdit(true)
+      }
+      else {
+        SetIsEdit(false)
+      }
+    }, [props])
+
+  function press() {
+    SetFormHidden(!FormHidden)
+  };
+
+  function firstNameChange(event) {
+    let task = Object.assign({}, Task);
+    task.firstName = event.target.value;
+    SetTask(task);
+
+    const isValid = inputsValidation();
+  };
+
+  function lastNameChange(event) {
+    let task = Object.assign({}, Task);
+    task.lastName = event.target.value;
+    SetTask(task);
+
+    const isValid = inputsValidation();
+  };
+
+  function emailChange(event) {
+    let task = Object.assign({}, Task);
+    task.email = event.target.value;
+    SetTask(task);
+
+    const isValid = inputsValidation();
+  };
+
+  function fromDateChange(event) {
+    let task = Object.assign({}, Task);
+    task.fromDate = event.target.value;
+    SetTask(task);
+  };
+
+  function toDateChange(event) {
+    let task = Object.assign({}, Task);
+    task.toDate = event.target.value;
+    SetTask(task);
+  };
+
+  function typeChange(value) {
+    let task = Object.assign({}, Task);
+    task.type = value.target.value;
+    SetTask(task);
+  };
+
+  function moreInfo(event) {
+    SetOtherFieldsVisible(!OtherFieldsVisible)
+  };
+
+  function reportChange(event) {
+    let task = Object.assign({}, Task);
+    task.report = !task.report;
+    SetTask(task);
   }
 
-  componentWillReceiveProps(){
-      console.log(this.props)
+  function commentChange(event) {
+    let task = Object.assign({}, Task);
+    task.comment = event.target.value;
+    SetTask(task);
+  };
 
-        if(this.props.edit){
-          this.setState(state => {
-              state.edit = this.props.edit;
-              state.task = this.props.changeableTask;
-              return state;
-            });
-        }
+  const inputsValidation = () => {
+    let task = Object.assign({}, Task);
+    const firstNameErr = {};
+    const lastNameErr = {};
+    const emailErr = {};
+    const regex = /^\S+@\S+\.\S+$/;
+    let isValid = true;
+
+    if(!task.firstName) {
+      firstNameErr.noFirstName = "Enter your first name";
+      isValid = false;
     }
 
-  press = () => {
-    this.setState({ formVisible: !this.state.formVisible });
-  };
+    if(!task.lastName) {
+      lastNameErr.noFirstName = "Enter your last name";
+      isValid = false;
+    }
 
-  firstNameChange = (event) => {
-    this.setState((state) => {
-      let { task } = state;
-      task.firstName = event.target.value;
-      return task;
-    });
-  };
+    if(!regex.test(task.email)) {
+      emailErr.emailIncorrect = "Incorrect email";
+      isValid = false;
+    }
 
-  lastNameChange = (event) => {
-    this.setState((state) => {
-      let { task } = state;
-      task.lastName = event.target.value;
-      return task;
-    });
-  };
+    setFirstNameErr(firstNameErr);
+    setLastNameErr(lastNameErr);
+    setEmailErr(emailErr);
 
-  emailChange = (event) => {
-    const patternEmail= "^[a-zA-Z0-9]{3,15}@[a-z]{2,}.(com|ru)$";
-    this.setState((state) => {
-      let { task } = state;
-      task.email = event.target.value;
-      return task;
-    });
-    const correct = valideteEmail(patternEmail, event.target.value);
-    if(!correct) return;
-  };
-
-  fromDateChange = (event) => {
-    this.setState((state) => {
-      let { task } = state;
-      task.fromDate = event.target.value;
-      return task;
-    });
-  };
-
-  toDateChange = (event) => {
-    this.setState((state) => {
-      let { task } = state;
-      task.toDate = event.target.value;
-      return task;
-    });
-  };
-
-  typeChange = (value) => {
-    this.setState((state) => {
-      let { task } = state;
-      task.type = value;
-      return task;
-    });
-  };
-
-  moreInfo = (event) => {
-    this.setState({ otherFields: !this.state.otherFields });
-  };
-
-  reportChange = (event) => {
-    this.setState((state) => {
-        let { task } = state;
-        task.report = !event.target.value;
-        return task;
-      });
+    return isValid;
   }
 
-  commentChange = (event) => {
-    this.setState((state) => {
-      let { task } = state;
-      task.comment = event.target.value;
-      return task;
+  function addTaskOrEdit() {
+    const isValid = inputsValidation();
+    if(!isValid) return;
+    props.addTaskOrEdit(Task);
+    SetTask({
+      id: Math.random(),
+      firstName: "",
+      lastName: "",
+      email: "",
+      fromDate: new Date(),
+      toDate: new Date(),
+      type: "",
+      report: false,
+      comment: "",
     });
+    SetFormHidden(!FormHidden);
   };
 
-  addTask = () => {
-    this.props.addNewTask(this.state.task);
-    this.setState((state) => {
-      state.task = {
-        id: Math.random(7),
-        firstName: "",
-        lastName: "",
-        email: "",
-        fromDate: new Date(),
-        toDate: new Date(),
-        type: "",
-        report: false,
-        comment: "",
-      };
-      state.formVisible = !state.formVisible;
-    });
-  };
+  return (
+    <div>
+      <button className="mainBtn" onClick={press}>
+        +
+      </button>
+      <div hidden={FormHidden} style={{ border: IsEdit ? 'solid 1px red' : '', padding: 5, margin: 5 }}>
+        <label>
+          First name:
+            <input
+            style={styles.input}
+            type="text"
+            value={Task.firstName}
+            onChange={firstNameChange}
+          />
+        </label>
+        {Object.keys(firstNameErr).map(key=>{
+          return <div style={{color: 'red'}}>{firstNameErr[key]}</div>
+        })}
+        <label>
+          Last name:
+            <input
+            style={styles.input}
+            type="text"
+            value={Task.lastName}
+            onChange={lastNameChange}
+          />
+        </label>
+        {Object.keys(lastNameErr).map(key=>{
+          return <div style={{color: 'red'}}>{lastNameErr[key]}</div>
+        })}
+        <label>
+          Email:
+            <input
+            style={styles.input}
+            type="email"
+            value={Task.email}
+            onChange={emailChange}
+          />
+        </label>
+        {Object.keys(emailErr).map(key=>{
+          return <div style={{color: 'red'}}>{emailErr[key]}</div>
+        })}
+        <label>
+          from:
+            <input
+            style={styles.input}
+            type="date"
+            checked={Task.fromDate}
+            onChange={fromDateChange}
+          />
+        </label>
+        <label>
+          to:
+            <input
+            style={styles.input}
+            type="date"
+            value={Task.toDate}
+            onChange={toDateChange}
+          />
+        </label>
+        <label>
+          type:&nbsp;
+            <select style={styles.select} value={Task.type} onChange={typeChange}>
+            <option defaultValue>1</option>
+            <option>2</option>
+            <option>3</option>
+          </select>
+        </label>
 
-  render() {
-    return (
-      <div>
-        <button className="mainBtn" onClick={this.press}>
-          +
-        </button>
-        <div hidden={this.state.formVisible}>
-          <label>
-            First name:
-            <input
-              style={styles.input}
-              type="text"
-              value={this.state.task.firstName}
-              onChange={this.firstNameChange}
-              required
-            />
-          </label>
-          <label>
-            Last name:
-            <input
-              style={styles.input}
-              type="text"
-              value={this.state.task.lastName}
-              onChange={this.lastNameChange}
-              required
-            />
-          </label>
-          <label>
-            Email:
-            <input
-              style={styles.input}
-              type="email"
-              value={this.state.task.email}
-              onChange={this.emailChange}
-              required
-            />
-          </label>
-          <label>
-            from:
-            <input
-              style={styles.input}
-              type="date"
-              value={this.state.task.fromDate}
-              onChange={this.fromDateChange}
-            />
-          </label>
-          <label>
-            to:
-            <input
-              style={styles.input}
-              type="date"
-              value={this.state.task.toDate}
-              onChange={this.toDateChange}
-            />
-          </label>
-          <label>
-            type:&nbsp;
-            <select style={styles.select} value={this.state.task.type} onChange={this.typeChange}>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-            </select>
-          </label>
+        <input type="button" value="more" onClick={moreInfo} />
 
-          <input type="button" value="more" onClick={this.moreInfo} />
-
-          <div hidden={this.state.otherFields}>
-            <div>
-              <input style={styles.report} 
-                value={this.state.report}
-                type="checkbox" 
-                onClick={this.reportChange} />
+        <div hidden={OtherFieldsVisible}>
+          <div>
+            <input style={styles.report}
+              type="checkbox"
+              onClick={reportChange} />
               make report
-            </div>
-            <label>
-              Comment:
-              <textarea style={styles.input}
-                value={this.state.comment}
-                onChange={this.commentChange}
-              />
-            </label>
           </div>
-          <input type="button" onClick={this.addTask} value="Add" />
+          <label>
+            Comment:
+              <textarea style={styles.input}
+              value={Task.comment}
+              onChange={commentChange}
+            />
+          </label>
         </div>
+        <input type="button" onClick={addTaskOrEdit} value={IsEdit ? "Save" : "Add"} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default AddTask;
